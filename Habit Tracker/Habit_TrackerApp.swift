7749c8a6de0cd13650bb217fe4e4dbcb2128839f
Finlay C.M <j>
2023-08-11 16:16:46 +0100

    Initial Commit

---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Finlay Carson Moretti on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Habit TrackerUITests/Habit_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Habit_TrackerUITestsLaunchTests.swift
+//  Habit TrackerUITests
+//
+//  Created by Finlay Carson Moretti on 11/08/2023.
+//
+
+import XCTest
+
+final class Habit_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
